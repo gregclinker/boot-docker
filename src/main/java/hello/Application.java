4f00e214903109c3a2c3d707371dbe4da52f87ef
gregclinker <gregory.clinker@gmail.com>
2017-02-20 14:24:42 +0000

    change

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Greg's Docker World";
     }
 
     public static void main(String[] args) {
