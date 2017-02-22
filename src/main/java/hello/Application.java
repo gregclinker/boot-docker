dc8181a59c4167d197ffc637ec76c6ff99844949
gregclinker <gregory.clinker@gmail.com>
2017-02-22 08:30:09 +0000

    format

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
     @RequestMapping("/")
     public String home() {
         return "Greg's Docker World";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/hello/HelloWorldConfigurationTests.java
+++ src/test/java/hello/HelloWorldConfigurationTests.java
@@ -15,8 +15,6 @@
  */
 package hello;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +26,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
